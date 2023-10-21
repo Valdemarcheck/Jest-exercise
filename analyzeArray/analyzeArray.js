8878b7b3d700cff93bcee7c3b8ff11283ee9943c
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-10-21 21:53:29 +0500

    Throw errors when given array has values besides numbers

--- analyzeArray/analyzeArray.js
+++ analyzeArray/analyzeArray.js
@@ -1,3 +1,9 @@
+function doesArrayContainOnlyNumbers(array) {
+  return array.every((value) => typeof value === "number");
+}
+
 export function analyzeArray(array) {
   if (!(array instanceof Array)) throw new Error("Given value isn't an array");
+  if (!doesArrayContainOnlyNumbers(array))
+    throw new Error("An array must contain only numbers");
 }
--- analyzeArray/analyzeArray.test.js
+++ analyzeArray/analyzeArray.test.js
@@ -5,3 +5,9 @@ test("Throws an error if given value isn't an instance of Array class", () => {
     analyzeArray("Not an array");
   }).toThrow();
 });
+
+test("Throws an error if given array contains other values besides numbers", () => {
+  expect(() => {
+    analyzeArray([0, "Hey", 3, 4, -5]);
+  }).toThrow();
+});
